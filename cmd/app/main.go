56856c31497abb10f6215776c49505cb011f96e4
Marcos Huck <marcos@huck.com.ar>
2021-01-20 12:29:50 -0300

    Add example.

---
+++ cmd/app/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/marcoshuck/runner"
+)
+
+func main() {
+	job := runner.NewJob()
+
+	job.WithPreHook(print).WithExecute(print).WithPostHook(print).WithErrorHandler(print)
+
+	output, err := job.Run(runner.Deployment{
+		Input: "Some test",
+		Error: nil,
+	})
+
+	fmt.Println("Output:", output)
+	fmt.Println("Error:", err)
+}
+
+func print(r runner.Deployment) (interface{}, error) {
+	fmt.Println("Printing request", r.Input)
+
+	r.Input = fmt.Sprintf("%v - printed", r.Input)
+
+	return r.Input, nil
+}
